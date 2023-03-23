cfbf299eb618810cf938ef433666723913cee05e
Henry Ihenacho <henrychris2003@gmail.com>
2023-03-23 23:47:20 +0100

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	if (size <= 0)
 	{
@@ -16,7 +16,11 @@ void print_triangle(int size)
 	}
 	for (i = 1; i <= size; i++)
 	{
-		for (j = 1; j <= i; j++)
+		for (j = 1; j <= size - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= i; k++)
 		{
 			_putchar('#');
 		}
