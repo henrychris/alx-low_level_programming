ed83bae6db1d9c60d707b6c6b0818f7c02b99cc3
Henry Ihenacho <henrychris2003@gmail.com>
2023-03-23 21:27:24 +0100

    1,2,3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - a function that multiplies two integers.
+ * @a: the  numbers being added
+ * @b: the  numbers being added
+ * Return: the product
+*/
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
