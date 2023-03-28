3d1f8084dac96ecebc423440228226900d475b48
Henry Ihenacho <henrychris2003@gmail.com>
2023-03-28 01:28:05 +0100

    fast

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string variable
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int length = _strlen(s);
+	char new_string[length + 1];
+	int max_size = length + 1;
+	char c;
+	int i = 0;
+
+	while (length > 0 && i < max_size)
+	{
+		c = *(s + length - 1);
+		new_string[i] = c;
+		length--;
+		i++;
+	}
+
+	/* Copy reversed string into original*/
+	for (int j = 0; j < i; j++)
+	{
+		*(s + j) = new_string[j];
+	}
+}
+
+/**
+ * _strlen - calculate string length without \0
+ * @s: the array variable
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int count = 1;
+	int length = 0;
+	char c = *s;
+
+	while (c != '\0')
+	{
+		c = *(s + count);
+		count++;
+		length++;
+	}
+
+	return (length);
+}
