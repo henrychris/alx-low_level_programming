54d349ef202ab5791236723bbf60e3b73853deb6
Henry Ihenacho <henrychris2003@gmail.com>
2023-03-28 23:26:08 +0100

    fast

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - print n elements of an array
@@ -8,40 +8,16 @@
  */
 void print_array(int *a, int n)
 {
-	int i, num, digits, k, temp, j, digit;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		num = *(a + i);
-		temp = num;
-		while (temp != 0)
-		{
-			digits++;
-			temp /= 10;
-		}
-
-		if (num < 0)
-		{
-			num = -num;
-			_putchar('-');
-		}
-
-		for (j = digits; j > 0; j--)
-		{
-			int divisor = 1;
-
-			for (k = 1; k < j; k++)
-			{
-				divisor *= 10;
-			}
-			digit = num / divisor % 10;
-			_putchar(digit + '0');
-		}
+		printf("%d", *(a + i));
 
 		if (i != n - 1)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
 	}
+	printf("\n");
 }
