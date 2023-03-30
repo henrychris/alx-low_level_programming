927584ed502cbf340546a402f1a3bc259def4971
Henry Ihenacho <henrychris2003@gmail.com>
2023-03-30 21:08:03 +0100

    fast

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -17,20 +17,17 @@ int _strcmp(char *s1, char *s2)
 		return (check);
 	}
 
-	/* from here, it checks differences in characters*/
-	check = 0;
-
 	for (i = 0; *(s1 + i) != '\0' && *(s2 + i) != '\0'; i++)
 	{
 		if (*(s1 + i) > *(s2 + i))
 		{
-			check++;
+			return (1);
 		}
-		else
-			check--;
+		else if (*(s1 + i) < *(s2 + i))
+			return (-1);
 	}
 
-	return (check == 0 ? 0 : (check > 0 ? 1 : -1));
+	return (0);
 }
 
 /**
