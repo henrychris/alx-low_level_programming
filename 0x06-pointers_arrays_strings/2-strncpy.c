fac2da1db945d991e83b57e971551e6bae63ad15
Henry Ihenacho <henrychris2003@gmail.com>
2023-03-30 20:38:21 +0100

    fast

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -29,15 +29,17 @@ int _strlen(char *s)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && *(src + i) != '\0'; i++)
+	while (i < n && (*(src + i) != '\0'))
 	{
 		*(dest + i) = *(src + i);
+		i++;
 	}
 	while (i < n)
 	{
 		*(dest + i) = '\0';
+		i++;
 	}
 	return (dest);
 }
