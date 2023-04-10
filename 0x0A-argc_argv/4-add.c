5ca543aa26bc6de94d98640f014db20a7e9089bc
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-10 12:34:25 +0100

    fast

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,13 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-
-	if (argc == 1)
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
-
+	
 	for (i = 1; i < argc; i++)
 	{
 		int val = atoi(argv[i]);
