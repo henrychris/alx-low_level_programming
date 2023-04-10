3e7a2005b7eed4c2ffa47a7dbab0bca3e402f86e
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-10 17:09:48 +0100

    fast

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,26 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - calculate string length without \0
- * @s: the array variable
- * Return: Length of the string
- */
-int _strlen(char *s)
-{
-	int count = 1;
-	int length = 0;
-	char c = *s;
-
-	while (c != '\0')
-	{
-		c = *(s + count);
-		count++;
-		length++;
-	}
-	return (length);
-}
-
-/**
  * _strcat - concatenates two strings
  * @dest: string being appended to
  * @src: string to be appended
@@ -28,15 +8,4 @@ int _strlen(char *s)
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = _strlen(dest);
-	int j = 0;
-
-	while ((*(src + j) != '\0'))
-	{
-		*(dest + i) = *(src + j);
-		i++;
-		j++;
-	}
-	*(dest + i) = '\0';
-	return (dest);
 }
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -8,36 +8,4 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int length = _strlen(src);
-	int i;
-	char c;
-
-	for (i = 0; i <= length; i++)
-	{
-		c = *(src + i);
-		*(dest + i) = c;
-	}
-
-	return (dest);
-}
-
-/**
- * _strlen - calculate string length without \0
- * @s: the array variable
- * Return: Length of the string
- */
-int _strlen(char *s)
-{
-	int count = 1;
-	int length = 0;
-	char c = *s;
-
-	while (c != '\0')
-	{
-		c = *(s + count);
-		count++;
-		length++;
-	}
-
-	return (length);
 }
