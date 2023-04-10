9007a2c96ed72c40e03529f0b7ea3498162b28fe
henrychris <64085210+henrychris@users.noreply.github.com>
2023-04-10 17:15:53 +0100

    got c files back,  removed strlen

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,25 +1,4 @@
 #include "main.h"
-
-/**
- * _strlen - calculate string length without \0
- * @s: the array variable
- * Return: Length of the string
- */
-int _strlen(char *s)
-{
-	int count = 1;
-	int length = 0;
-	char c = *s;
-
-	while (c != '\0')
-	{
-		c = *(s + count);
-		count++;
-		length++;
-	}
-	return (length);
-}
-
 /**
  * _strncpy - copies a string
  * @dest: destination string
