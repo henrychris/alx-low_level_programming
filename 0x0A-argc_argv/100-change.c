22292fc90d5ebbdd11b00bbb4c6ba78a7e553b18
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-10 14:47:53 +0100

    fast

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,150 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+#pragma region
+int main(int argc, char *argv[])
+{
+	int i, cents, coin_count = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	if (!cents)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	if (cents < TWENTY_FIVE_CENTS)
+	{
+		printf("%d", handle_ten_cents(cents, 0));
+	}
+	else
+		printf("%d", handle_25_cents(cents, 0));
+
+	return (0);
+}
+#pragma endregion
+
+/**
+ * handle_25_cents - calculates the minimum number of
+ * coins required for 25 cents
+ * @cents: the amount of money left in cents
+ * @count: the current count of coins
+ * Return: the minimum number of coins required for 25 cents
+ */
+int handle_25_cents(int cents, int count)
+{
+	/* Calculate the remainder after dividing by 25 cents */
+	int rem = cents % TWENTY_FIVE_CENTS;
+
+	/*
+	 * If the remainder is not zero, handle
+	 * the remaining coins with handle_ten()
+	 */
+	if (rem)
+	{
+		/* how many times does 25 cents go in cents*/
+		int times = cents / TWENTY_FIVE_CENTS;
+		/* subtract 25 that many times */
+		cents -= TWENTY_FIVE_CENTS * times;
+		return (handle_ten_cents(cents, times));
+	}
+	/* add the current coin count */
+	return ((cents / TWENTY_FIVE_CENTS) + count);
+}
+
+/**
+ * handle_ten_cents - calculates the minimum number
+ * of coins required for 10 cents
+ * @cents: the amount of money left in cents
+ * @count: the current count of coins
+ * Return: the minimum number of coins required for 10 cents
+ */
+int handle_ten_cents(int cents, int count)
+{
+	/*
+	 * If the money is less than 10 cents,
+	 * handle the remaining coins with handle_ten()
+	 */
+	if (cents < TEN_CENTS)
+	{
+		return (handle_five_cents(cents, count));
+	}
+
+	int rem = cents % TEN_CENTS;
+
+	if (rem)
+	{
+		int times = cents / TEN_CENTS;
+
+		cents -= TEN_CENTS * times;
+		return (handle_five_cents(cents, times + count));
+	}
+	return ((cents / TEN_CENTS) + count);
+}
+
+/**
+ * handle_five_cents - calculates the minimum number
+ * of coins required for 5 cents
+ * @cents: the amount of money left in cents
+ * @count: the current count of coins
+ * Return: the minimum number of coins required for 5 cents
+ */
+int handle_five_cents(int cents, int count)
+{
+	if (cents < FIVE_CENTS)
+		return (handle_two_cents(cents, count));
+
+	int rem = cents % FIVE_CENTS;
+
+	if (rem)
+	{
+		int times = cents / FIVE_CENTS;
+
+		cents -= FIVE_CENTS * times;
+		return (handle_five_cents(cents, times + count));
+	}
+	return ((cents / FIVE_CENTS) + count);
+}
+
+/**
+ * handle_two_cents - calculates the number
+ * of 2-cent coins needed to make change
+ * @cents: the total number of cents left to make change for
+ * @count: the current count of coins
+ * Return: the total count of coins needed to make change
+ */
+int handle_two_cents(int cents, int count)
+{
+	/*
+	 * if it's less than two cents,
+	 * return the amount of 1 cent coins
+	 */
+	if (cents < TWO_CENTS)
+		return ((cents / ONE_CENT) + count);
+
+	int rem = cents % TWO_CENTS;
+
+	if (rem)
+	{
+		int times = cents / TWO_CENTS;
+
+		cents -= TWO_CENTS * times;
+		return ((cents / ONE_CENT) + times + count);
+	}
+	return ((cents / TWO_CENTS) + count);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,4 +3,14 @@
 
 int _putchar(char c);
 
+const int TWENTY_FIVE_CENTS = 25;
+const int TEN_CENTS = 10;
+const int FIVE_CENTS = 5;
+const int TWO_CENTS = 2;
+const int ONE_CENT = 1;
+
+int handle_ten_cents(int cents, int count);
+int handle_25_cents(int cents, int count);
+int handle_five_cents(int cents, int count);
+int handle_two_cents(int cents, int count);
 #endif
