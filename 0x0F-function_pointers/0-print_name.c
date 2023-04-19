c7866f4d3829cad29a47a96c321f614210f34784
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-19 21:50:58 +0100

    fast

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to a function that prints a name
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
