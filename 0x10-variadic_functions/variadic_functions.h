92a8e26ee6a79a54fe01a3e17f2ae0e9ea80a40c
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-21 03:00:32 +0100

    fast

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-void print_format(const char *format, int i, va_list args);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
