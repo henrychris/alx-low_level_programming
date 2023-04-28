85cfad825186c8141c7d2f6c2e28df751cab4f68
Henry Ihenacho <henrychris2003@gmail.com>
2023-04-28 05:22:16 +0100

    fast

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: pointer to the list_t list
+ * Return: the number of elements in the list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	if (h == NULL)
+	{
+		return (nodes);
+	}
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
