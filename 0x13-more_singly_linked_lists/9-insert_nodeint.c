3d686314bdb1bf07cf62ef7bf1f1c5faa63d8242
Henry Ihenacho <henrychris2003@gmail.com>
2023-05-02 21:39:26 +0100

    fast

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *new_node;
 	unsigned int i = listint_len(*head);
 
-	if (head == NULL || *head == NULL || idx > i)
+	if (head == NULL || idx > i)
 		return (NULL);
 
 	if (idx == 0)
