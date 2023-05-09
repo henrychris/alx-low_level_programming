81d57372a4d68b99eb07032e1d6783f65af01ac3
Henry Ihenacho <henrychris2003@gmail.com>
2023-05-09 05:07:39 +0100

    fast

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t length, status = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "\0";
+
+	length = strlen(text_content);
+	status += write(fd, text_content, length);
+	if (status < length)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
