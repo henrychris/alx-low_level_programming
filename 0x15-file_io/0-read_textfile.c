4fcba39fd9048f3d95197988b21ac7b555cb82ed
Henry Ihenacho <henrychris2003@gmail.com>
2023-05-09 05:27:32 +0100

    fast

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,25 +8,33 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_read = 0, bytes_written = 0, letters2 = letters;
-	char buffer[1024];
+	ssize_t bytes_read = 0, bytes_written = 0;
+	char *buffer;
 	int fd;
 
 	if (!filename)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (fd < 0)
 		return (0);
 
-	while ((bytes_read = read(fd, buffer, sizeof(letters))) > 0
-			&& bytes_written < letters2)
+	buffer = malloc(letters);
+	if (!buffer)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read < 0)
 	{
-		bytes_written += write(STDOUT_FILENO, buffer, bytes_read);
-		if (bytes_written == -1)
-			return (0);
+		free(buffer);
+		return (0);
 	}
 
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written < 0)
+		return (0);
+
 	close(fd);
+	free(buffer);
 	return (bytes_written);
 }
